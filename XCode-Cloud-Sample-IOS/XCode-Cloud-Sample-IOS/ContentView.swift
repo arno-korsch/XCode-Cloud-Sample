25c8904c397497e39f2714e0590da88741f6ff71
Arno <rathmann@korsch.de>
2023-12-22 09:16:07 +0100

    Initial Commit

---
+++ XCode-Cloud-Sample-IOS/XCode-Cloud-Sample-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XCode-Cloud-Sample-IOS
+//
+//  Created by Arno Rathmann on 22.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world Cloud!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XCode-Cloud-Sample-IOS/XCode-Cloud-Sample-IOS/XCode_Cloud_Sample_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCode_Cloud_Sample_IOSApp.swift
+//  XCode-Cloud-Sample-IOS
+//
+//  Created by Arno Rathmann on 22.12.23.
+//
+
+import SwiftUI
+
+@main
+struct XCode_Cloud_Sample_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
